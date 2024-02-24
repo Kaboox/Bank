d856681facedd1e43f0e4cb4e59a9e2d48a20a39
Kabox <kabox9@gmail.com>
2024-02-24 19:20:55 +0100

    basic encapsulation structure for account class

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -5,4 +5,27 @@ public class Account {
     private String password;
     private int account_type;
 
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getAccount_type() {
+        return account_type;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAccount_type(int account_type) {
+        this.account_type = account_type;
+    }
 }
